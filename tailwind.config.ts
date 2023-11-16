de0cd0018ce66471587d2ac3b6c840f26a09c207
Andri Valur Guðjohnsen <wolfninja13@gmail.com>
2023-11-16 17:55:41 +0000

    new home page with some tailwind component I found

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'icelandic-blue': '#02539c',
+        'icelandic-red': '#d91f32',
+      },
     },
   },
   plugins: [],
